77fc94e7787742f9d2fee7872f7ad5014765c65a
shen-dsh <1114325898@qq.com>
2020-08-28 19:08:15 +0800

    搭建Springboot项目作为基础

---
+++ codeRoad/src/main/java/com/example/codeRoad/CodeRoadApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeRoad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeRoadApplication.class, args);
+	}
+
+}
---
+++ codeRoad/src/test/java/com/example/codeRoad/CodeRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeRoad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeRoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
